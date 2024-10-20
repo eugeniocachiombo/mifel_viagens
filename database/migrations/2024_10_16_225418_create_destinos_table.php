6338b520ed4d3e7a15a82af2a986d5a734cb35ed
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-10-20 02:30:45 +0100

    Descrições das provinciais

--- database/migrations/2024_10_16_225418_create_destinos_table.php
+++ database/migrations/2024_10_16_225418_create_destinos_table.php
@@ -27,10 +27,10 @@ class CreateDestinosTable extends Migration
         });
 
 
-        $this->createDestino("Luanda", "Capital de Angola", "assets/images/provincias/Luanda.jpg");
-        $this->createDestino("Benguela", "...", "assets/images/provincias/benguela.jpg");
-        $this->createDestino("Huíla", "...", "assets/images/provincias/huila.jpg");
-        $this->createDestino("Namibe", "...", "assets/images/provincias/namibe.jpg");
+        $this->createDestino("Luanda", "Luanda é a capital de Angola, situada na costa atlântica do país. É uma cidade vibrante e em crescimento, marcada por contrastes entre áreas modernas e tradicionais. O porto de Luanda é um dos mais importantes da região.", "assets/images/provincias/Luanda.jpg");
+        $this->createDestino("Benguela", "Benguela é uma cidade costeira de Angola, conhecida por suas belas praias e clima ameno. É um importante centro econômico e cultural, com uma rica história colonial.", "assets/images/provincias/benguela.jpg");
+        $this->createDestino("Huíla", "Huíla é uma província de Angola, localizada no sul do país. Sua capital, Lubango, é famosa pela impressionante Serra da Leba e pela estátua do Cristo Rei, que oferece vistas panorâmicas deslumbrantes.", "assets/images/provincias/huila.jpg");
+        $this->createDestino("Namibe", "Namibe é uma província costeira de Angola, conhecida por suas impressionantes paisagens desérticas e litoral deslumbrante. A capital, também chamada Namibe, destaca-se por suas praias, como a Praia do Dúbia.", "assets/images/provincias/namibe.jpg");
     }
 
     public function createDestino($nome_destino, $desc_destino, $img_destino)
